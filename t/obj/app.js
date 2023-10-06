85b7d5168d9f81474eb375173157e7c89aada57c
Aliteymourinia <aliteywb@gmail.com>
2023-10-06 19:42:10 +0330

    formated

---
+++ t/obj/app.js
@@ -0,0 +1,9 @@
+const nameEl = document.getElementById("name");
+const lastNameEl = document.getElementById("lastName");
+// const person = {
+//   name: "last",
+//   family: "hey",
+// };
+// const key = "age";
+// person[key] = 20;
+// console.log(person);
