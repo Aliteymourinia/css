d3021de384f37b8bd096f26815d7e70f137aa6dc
Aliteymourinia <aliteywb@gmail.com>
2023-10-06 18:48:37 +0330

    add tailwind

---
+++ t/app.js
@@ -0,0 +1,10 @@
+const nameEl = document.getElementById("name");
+const lastNameEl = document.getElementById("lastName");
+
+const data = {
+  firstName: "gggd",
+  lastName: "teimory",
+};
+
+nameEl.textContent = data.firstName;
+lastNameEl.textContent = data.temp;
