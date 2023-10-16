4b4a572e3abd073c98b7d918f21405813d93ebd9
Aliteymourinia <aliteywb@gmail.com>
2023-10-16 18:38:59 +0330

    add some changes

--- t/logic/app.js
+++ t/logic/app.js
@@ -1,9 +1,21 @@
-const input = document.querySelector("input");
-const button = document.querySelector("button");
+// function sayInfo(name, age) {
+//   const msg = "your name is " + name + " and age is " + age;
+//   alert(msg);
+// }
+// sayInfo("Ali", 22);
 
-input.addEventListener("input", function (e) {
-  console.log(e.target.value);
-});
-input.addEventListener(('submit'),()=>{
-  
-});
+// const sayName = (name, age) => {
+//   alert("your name is " + name + " your age is" + age);
+// };
+// sayName("navid", 10);
+// const sayNameWith = (name, age) =>
+//   alert(`your name is ${name} your age is ${age}`);
+// sayNameWith("navid inter", 10);
+
+// const return2 = () => 2;
+// const wrongReturn = () => {
+//   return 2;
+// };
+// console.log(return2(), wrongReturn());
+const birtday=1995;
+alert("birthday year is 1995")
